72a9ba0739a0dc0c8d213e5dccd4b09f347e908d
SanyamMadaan <124160436+SanyamMadaan@users.noreply.github.com>
2024-04-26 15:42:30 +0530

    Delete FED Project directory

--- FED Project/Readme.md
+++
@@ -1,2 +0,0 @@
-![image](https://github.com/SanyamMadaan/FED/assets/124160436/35a0852a-66d6-4927-9981-568c6c39599c)
-![image](https://github.com/SanyamMadaan/FED/assets/124160436/949293e7-4676-4078-9fbb-e8e7891c1e7e)
--- FED Project/backend/helper/generateToken.js
+++
@@ -1,8 +0,0 @@
-const jwt = require("jsonwebtoken");
-
-exports.generateToken = async (id) => {
-  let token = jwt.sign({ id }, process.env.JWT_SECRET, {
-    expiresIn: "24h", // expires in 24 hours
-  });
-  return token;
-};
